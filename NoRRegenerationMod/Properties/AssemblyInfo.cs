8307d80bfcded777f36cd074fea59258517381a2
OleksandrMoskaliuk <redownloader999@gmail.com>
2026-09-02 16:10:45 +0300

    files restructure

--- NoRRegenerationMod/Properties/AssemblyInfo.cs
+++ NoRRegenerationMod/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NoRRegenerationMod")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("80bc5fd6-41c0-4e27-93ab-10e4cfc6d4c5")]
+[assembly: Guid("b5384f20-742b-405a-9c42-fd5d712efebd")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ NoRRegenerationMod/README.md
@@ -0,0 +1,21 @@
+
+
+## Prerequisites
+
+- **[BepInEx v5.4.23.5](https://github.com/bepinex/bepinex/releases)**: Requires the **x86 (32-bit)** version for Windows. Note: The x64 (64-bit) version is not supported.
+
+---
+
+## Installation
+
+1. Download and install **BepInEx v5.4.23.5 (x86)** into your game directory.
+2. Run the game once to allow BepInEx to generate its folder structure.
+3. Place `NoRParryMod.dll` into the `BepInEx/plugins` directory.
+
+---
+
+## Configuration
+
+After launching the game once with the mod installed, a configuration file named `NoRParryMod.cfg` will be generated in `BepInEx/config/`.
+
+Open this file in any text editor to customize your block and parry thresholds and timing settings.
