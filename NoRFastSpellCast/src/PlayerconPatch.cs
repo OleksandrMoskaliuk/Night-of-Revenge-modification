4c81454d44f876f445a519c08a4f809d12d2f030
OleksandrMoskaliuk <redownloader999@gmail.com>
2026-09-02 18:30:52 +0300

    NoRFastSpellCastMod

---
+++ NoRFastSpellCast/src/PlayerconPatch.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace NoRFastSpellCast
+{
+
+    internal class PlayerConPatch
+    {
+
+        // Fast magic cast
+        [global::HarmonyLib.HarmonyPatch(typeof(global::playercon), "magic_fun")]
+        [global::HarmonyLib.HarmonyPrefix]
+        public static void FastMagicCast(global::playercon __instance, global::PlayerStatus ___playerstatus,
+             bool ___key_guard, bool ___Attacknow, int ___stepkind, bool ___nowdamage,
+             bool ___magicnow, bool ___Itemuse, bool ___Death, ref bool ___Parry,
+             ref float ___parrycount, ref float ___guradcount, float ___key_vertical, ref bool ___magicfire, ref float ___mgcount, ref int ___magicdatanum,
+             ref GameObject ___MagicSpellCanvas)
+        {
+            if (!__instance.nowdamage)
+            {
+                ___mgcount += ___playerstatus.Magicdata[__instance.maginumberfrag].cooltime * 0.6f * Time.deltaTime;
+            }
+        }
+
+
+        // Token: 0x06000012 RID: 18 RVA: 0x00002073 File Offset: 0x00000273
+        public PlayerConPatch()
+        {
+        }
+    }
+}
---
+++ NoRFastSpellCast/src/Plugin.cs
@@ -0,0 +1,74 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace NoRFastSpellCast
+{
+    [global::BepInEx.BepInPlugin("NoRFastMagicCast", "[twitter @Dru9Dealer] NoR_FastCast", "1.0.0")]
+    [global::BepInEx.BepInProcess("NightofRevenge.exe")]
+    public class Plugin : global::BepInEx.BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            global::HarmonyLib.Harmony.CreateAndPatchAll(typeof(global::NoRFastSpellCast.PlayerConPatch), null);
+            Plugin.LoggerMessage01 = "[Twitter @Dru9Dealer] FastCast mod enable";
+        }
+
+        private void Update()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+            HandleLoggers(true);
+        }
+
+        private void HandleLoggers(bool on)
+        {
+            if (!on) { return; }
+
+            // Logger 01            
+            if (LogDat1.TimeRamained > 0)
+            {
+                global::UnityEngine.GUI.Box(LogDat1.rectangle, " " + LoggerMessage01);
+                LogDat1.LastMessage = LoggerMessage01;
+                LogDat1.TimeRamained -= UnityEngine.Time.deltaTime;
+            }
+            // Update time if new message was assigned
+            if (!LoggerMessage01.Equals(LogDat1.LastMessage))
+            {
+                // Prevents messages flickering
+                if (LogDat1.TimeRamained > (5f - 0.8f))
+                {
+                    LoggerMessage01 = LogDat1.LastMessage;
+                }
+                else
+                {
+                    LogDat1.TimeRamained = 5f;
+                }
+            }
+        }
+        public Plugin()
+        {
+            // Logger 01
+            LogDat1.LastMessage = LoggerMessage01;
+            LogDat1.rectangle = new global::UnityEngine.Rect(10f, 240, 350f, 24f);
+
+        }
+
+        // Logger 01
+        public static string LoggerMessage01;
+        private LogData LogDat1;
+
+    }
+
+    // Data for Logging messages
+    public struct LogData
+    {
+        public Rect rectangle;
+        public string LastMessage;
+        public float TimeRamained;
+    }
+
+}
+
